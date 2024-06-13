a025f4437aaabb7302e3e88f5769c8ea7c33052e
hafiz-aamir <165315543+hafiz-aamir@users.noreply.github.com>
2024-06-13 06:39:50 -0700

    Update admin_school_step_1.blade.php

--- resources/views/custom_admin/admin_school_step_1.blade.php
+++ resources/views/custom_admin/admin_school_step_1.blade.php
@@ -245,6 +245,35 @@
   $("#next_btn").select2({
       placeholder: "Select a Coach",
       allowClear: true,
+
+
+      minimumInputLength: 2,
+            
+            ajax: {
+
+                url: "{{ route('autocomplete_coach') }}",
+                dataType: 'json',
+                delay: 250,
+                data: function (params) {
+                    return {
+                        query: params.term // search term
+                    };
+                },
+                processResults: function (data) {
+                    return {
+                        results: $.map(data, function (item) {
+                            return {
+                                text: item.name, // replace 'name' with the relevant field
+                                id: item.id
+                            }
+                        })
+                    };
+                },
+                cache: true
+
+            }
+            
+
   });
   
 
